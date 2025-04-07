7ba64067200b0aff3897b16bfff8033db60ff669
Estefern <09Pepita16!!>
2025-04-07 09:51:07 +0200

    Section 11

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -17,7 +17,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
-        log.debug("Getting Indewx page");
+        log.debug("Getting Index page");
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
